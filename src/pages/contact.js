d1bea95934a28d26987bde833ce19b36558e1c9b
rogfut <seasonalvegetables@outlook.com>
2020-02-10 23:09:05 -0800

    added azure-pipelines.yaml, some sass fixes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from '../components/layout'
+import { Link } from "gatsby"
 
 const Contact = () => {
     return (
         <Layout>
             <h1>Contact</h1>
+            <h3>You can find me on <Link to="linkedin.com/in/rogerfutrell">LinkedIn</Link> or email me at <a href="mailto: rfut@outlook.com">rfut@outlook.com</a></h3>
         </Layout>    
     )
 }
