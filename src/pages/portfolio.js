708e55e65ff7180bf05c1e8ab962226601dee64c
rogfut <seasonalvegetables@outlook.com>
2020-02-10 23:39:51 -0800

    commenting out unfinished portfolio divs

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -10,26 +10,26 @@ const Portfolio = () => {
             <div>
                 <h1 className={portfolioStyles.portfolioHeader}>Portfolio</h1>
                 <div className={portfolioStyles.portfolioParent}>
-                    <div className={portfolioStyles.portfolio1}>
+                    {/* <div className={portfolioStyles.portfolio1}> */}
                         {/* <figure>
                             <img src="../img/btc.png" alt="port-1" style="width:100%"></img>
                             <figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption>
                         </figure> */}
                     {/* <figcaption>Text Below Image</figcaption> */}
-                    <p>Text at bottom of image</p>
+                    {/* <p>Text at bottom of image</p>
                     </div>
                     <div className={portfolioStyles.portfolio2}>
                     {/* <figcaption>Text Below Image</figcaption> */}
-                    <p>Text at bottom of image</p>
-                    </div>
-                    <div className={portfolioStyles.portfolio3}>
+                    {/* <p>Text at bottom of image</p> */}
+                    {/* </div> */}
+                    {/* <div className={portfolioStyles.portfolio3}> */}
                     {/* <figcaption>Text Below Image</figcaption> */}
-                    <p>Text at bottom of image</p>
-                    </div>
-                    <div className={portfolioStyles.portfolio4}>
+                    {/* <p>Text at bottom of image</p> */}
+                    {/* </div> */}
+                    {/* <div className={portfolioStyles.portfolio4}> */}
                     {/* <figcaption>Text Below Image</figcaption> */}
-                    <p>Text at bottom of image</p>
-                    </div>
+                    {/* <p>Text at bottom of image</p> */}
+                    {/* </div> */}
                 </div>
             </div>
         </Layout>       
