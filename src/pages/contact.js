9f5f49170719fc5967257baac36ee75cece7748e
rogfut <rogerfutrell@outlook.com>
2020-06-14 01:08:06 -0700

    update portfolio flexbox

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,31 @@
 import React from "react"
 import Layout from '../components/layout'
+import contactStyles from '../components/contact.module.scss'
 import { Link } from "gatsby"
+import linkedin from "../img/LI-Logo.png"
+import github from "../img/gh-black.png"
+import email from "../img/email.png"
+
 
 const Contact = () => {
     return (
         <Layout>
-            <h1>Contact</h1>
-            <h3>You can find me on <a href="https://www.linkedin.com/in/rogerfutrell">LinkedIn</a> or email me at <a href="mailto: rfut@outlook.com">rfut@outlook.com</a></h3>
+            <div className={contactStyles.main}>
+                {/* <h1>Contact</h1> */}
+                <h3>You can find me on <a href="https://www.linkedin.com/in/rogerfutrell">LinkedIn</a> or email me at <a href="mailto: rfut@outlook.com">rfut@outlook.com</a></h3>
+                <div className={contactStyles.logos}>
+                    <div className={contactStyles.linkedin}>
+                        <img src={linkedin}></img>
+                    </div>
+                    <div className={contactStyles.email}>
+                        <img src={email}></img>
+                    </div>
+                    <div className={contactStyles.github}>
+                        <img src={github}></img>
+                    </div>
+                </div>
+            </div>
+            
         </Layout>    
     )
 }
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,34 +2,37 @@ import React from "react"
 import Layout from '../components/layout'
 import { Link } from 'gatsby'
 import portfolioStyles from '../components/portfolio.module.scss'
-
+import img1 from "../img/1.png"
+import img2 from "../img/2.png"
+import img3 from "../img/3.png"
+import img4 from "../img/4.png"
 
 const Portfolio = () => {
     return (
         <Layout>
-            <div>
-                <h1 className={portfolioStyles.portfolioHeader}>Portfolio</h1>
+            <div className={portfolioStyles.portfolioMain}>
+                {/* <h1 className={portfolioStyles.portfolioHeader}>Portfolio</h1> */}
                 <div className={portfolioStyles.portfolioParent}>
-                    {/* <div className={portfolioStyles.portfolio1}> */}
-                        {/* <figure>
-                            <img src="../img/btc.png" alt="port-1" style="width:100%"></img>
-                            <figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption>
-                        </figure> */}
-                    {/* <figcaption>Text Below Image</figcaption> */}
-                    {/* <p>Text at bottom of image</p>
+                    <div className={portfolioStyles.portfolioItem}>
+                        <a className={portfolioStyles.portfolioItem1} href="https://github.com/rogfut" target="_blank">
+                        <img className={portfolioStyles.portfolioImage} src={img1} alt="portfolio-item 1"></img>
+                        </a>
                     </div>
-                    <div className={portfolioStyles.portfolio2}>
-                    {/* <figcaption>Text Below Image</figcaption> */}
-                    {/* <p>Text at bottom of image</p> */}
-                    {/* </div> */}
-                    {/* <div className={portfolioStyles.portfolio3}> */}
-                    {/* <figcaption>Text Below Image</figcaption> */}
-                    {/* <p>Text at bottom of image</p> */}
-                    {/* </div> */}
-                    {/* <div className={portfolioStyles.portfolio4}> */}
-                    {/* <figcaption>Text Below Image</figcaption> */}
-                    {/* <p>Text at bottom of image</p> */}
-                    {/* </div> */}
+                    <div className={portfolioStyles.portfolioItem}>
+                        <a className={portfolioStyles.portfolioItem2} href="https://github.com/rogfut" target="_blank">
+                        <img className={portfolioStyles.portfolioImage} src={img2} alt="portfolio-item 2"></img>
+                        </a>
+                    </div>
+                    <div className={portfolioStyles.portfolioItem}>
+                        <a className={portfolioStyles.portfolioItem3} href="https://github.com/rogfut" target="_blank">
+                        <img className={portfolioStyles.portfolioImage} src={img3} alt="portfolio-item 3"></img>
+                        </a>
+                    </div>
+                    <div className={portfolioStyles.portfolioItem}>
+                        <a className={portfolioStyles.portfolioItem4} href="https://github.com/rogfut" target="_blank">
+                        <img className={portfolioStyles.portfolioImage} src={img4} alt="portfolio-item 4"></img>
+                        </a>
+                    </div>
                 </div>
             </div>
         </Layout>       
