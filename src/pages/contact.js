ebaf3a065f6d006906bb1e76716a95d661d551f7
rogfut <rogerfutrell@outlook.com>
2020-06-17 22:57:46 -0700

    some layout and color tweaks

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,7 +12,7 @@ const Contact = () => {
         <Layout>
             <div className={contactStyles.main}>
                 {/* <h1>Contact</h1> */}
-                <h3>You can find me on <a href="https://www.linkedin.com/in/rogerfutrell">LinkedIn</a> or email me at <a href="mailto: rfut@outlook.com">rfut@outlook.com</a></h3>
+                <h3>You can find me on <a href="https://www.linkedin.com/in/rogerfutrell" className={contactStyles.links}>LinkedIn</a> or email me at <a href="mailto: rfut@outlook.com" className={contactStyles.links}>rfut@outlook.com</a></h3>
                 <div className={contactStyles.logos}>
                     <div className={contactStyles.linkedin}>
                         {/* <img src={linkedin}></img> */}
