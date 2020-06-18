4235728bd82e35d4dde582d8c11eff5053fe4cf9
rogfut <rogerfutrell@outlook.com>
2020-06-17 23:21:28 -0700

    updated index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from '../components/layout'
 import indexStyles from "../components/index.module.scss"
+import { Link } from 'gatsby'
 
 const Index = () => {
     var taglineStyle = {
@@ -10,8 +11,8 @@ const Index = () => {
     return (
         <Layout>
             <h1>Hello!</h1>
-            <h3>DevOps Engineer living in the Greater Seattle area</h3>
-            <h3>Pardon my mess while I get this site up and running</h3>
+            <h3>I'm a DevOps Engineer living in the Greater Seattle area</h3>
+            <h3>Check out my <Link to="/portfolio" className={indexStyles.link}>portfolio</Link> or <Link to="/contact" className={indexStyles.link}>contact</Link> me to say hi!</h3>
         </Layout>
     )
 }
