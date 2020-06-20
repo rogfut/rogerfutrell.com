875ee33c00badb05c98bbb04e7e533c780d6fba2
rogfut <rogerfutrell@outlook.com>
2020-06-19 21:59:17 -0700

    updated portfolio links and description

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -14,21 +14,21 @@ const Portfolio = () => {
                 {/* <h1 className={portfolioStyles.portfolioHeader}>Portfolio</h1> */}
                 <div className={portfolioStyles.portfolioParent}>
                     <div className={portfolioStyles.portfolioItem1}>
-                        <a href="https://github.com/rogfut" target="_blank">
+                        <a href="https://github.com/rogfut/rogerfutrell.com" target="_blank">
                         <p>JAMSTACK static site hosted on Azure Storage.</p>
                         <h4>ROGERFUTRELL.COM</h4>
                         </a>
                     </div>
                     <div className={portfolioStyles.portfolioItem2}>
-                        <a href="https://github.com/rogfut" target="_blank">
+                        <a href="https://github.com/rogfut/dca-btc" target="_blank">
                         <p>Serverless bot that buys btc for me.</p>
                         <h4>BUY BTC BOT</h4>
                         </a>
                     </div>
                     <div className={portfolioStyles.portfolioItem3}>
-                        <a href="https://github.com/rogfut" target="_blank">
-                        <p>Coming soon! What could it be?</p>
-                        <h4>Myster Project :)</h4>
+                        <a href="https://github.com/rogfut/etch-a-sketch" target="_blank">
+                        <p>Drawing fun with jquery.</p>
+                        <h4>ETCH-A-SKETCH</h4>
                         </a>
                     </div>
                     <div className={portfolioStyles.portfolioItem4}>
